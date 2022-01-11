c78ddb6f099d7811ce48f50bf664582295275325
Anita Safar <ancsa0416@gmail.com>
2022-01-11 14:23:39 +0000

    engine setup, getters, setters done

--- src/main/java/components/Engine.java
+++ src/main/java/components/Engine.java
@@ -1,4 +1,18 @@
 package components;
 
 public class Engine {
+
+    private double size;
+
+    public Engine(double size) {
+        this.size = size;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
 }
