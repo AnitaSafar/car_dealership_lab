1c972b7ffd4a1ca48d7ce5aa1461ca8a3cb5c3ed
Anita Safar <ancsa0416@gmail.com>
2022-01-11 14:25:53 +0000

    battery setup, getters and setters done

--- src/main/java/components/Battery.java
+++ src/main/java/components/Battery.java
@@ -1,4 +1,28 @@
 package components;
 
 public class Battery {
+
+    private int size;
+    private String make;
+
+    public Battery(int size, String make) {
+        this.size = size;
+        this.make = make;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
 }
