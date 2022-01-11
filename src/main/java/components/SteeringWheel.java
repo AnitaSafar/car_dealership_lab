282bf8f30cbf2739665aad81f2bd342136480907
Anita Safar <ancsa0416@gmail.com>
2022-01-11 14:22:18 +0000

    steeringWheel getters and setters added

--- src/main/java/components/SteeringWheel.java
+++ src/main/java/components/SteeringWheel.java
@@ -9,5 +9,20 @@ public class SteeringWheel {
         this.size = size;
         this.material = material;
     }
-    
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
 }
