edbea936f9b55bab7d8485a4f5307d9d7ab48e12
Anita Safar <ancsa0416@gmail.com>
2022-01-11 14:25:04 +0000

    tyre setup, getters and setters added

--- src/main/java/components/Tyre.java
+++ src/main/java/components/Tyre.java
@@ -1,4 +1,28 @@
 package components;
 
 public class Tyre {
+
+    private String type;
+    private String size;
+
+    public Tyre(String type, String size) {
+        this.type = type;
+        this.size = size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
 }
