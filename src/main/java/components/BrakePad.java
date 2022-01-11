a8b9305445d5db63a9604a67e204f0f6d1285a99
Anita Safar <ancsa0416@gmail.com>
2022-01-11 14:26:51 +0000

    brakePad setup, getters and setters added

--- src/main/java/components/BrakePad.java
+++ src/main/java/components/BrakePad.java
@@ -1,4 +1,18 @@
 package components;
 
 public class BrakePad {
+
+    private String make;
+
+    public BrakePad(String make) {
+        this.make = make;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
 }
