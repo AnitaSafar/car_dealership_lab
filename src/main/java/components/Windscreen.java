914779934ee9cdbe05b80a681af4c1d4ec66f322
Anita Safar <ancsa0416@gmail.com>
2022-01-11 14:19:54 +0000

    windscreen getters and setters added

--- src/main/java/components/Windscreen.java
+++ src/main/java/components/Windscreen.java
@@ -9,5 +9,20 @@ public class Windscreen {
         this.size = size;
         this.material = material;
     }
-    
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
 }
