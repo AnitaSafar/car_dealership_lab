0a367fc8ef4a48e001327b58cec7c52ca9a68d82
Anita Safar <ancsa0416@gmail.com>
2022-01-11 14:18:57 +0000

    windscreen setup done

--- src/main/java/components/Windscreen.java
+++ src/main/java/components/Windscreen.java
@@ -1,4 +1,13 @@
 package components;
 
 public class Windscreen {
+
+    private String size;
+    private String material;
+
+    public Windscreen(String size, String material) {
+        this.size = size;
+        this.material = material;
+    }
+    
 }
