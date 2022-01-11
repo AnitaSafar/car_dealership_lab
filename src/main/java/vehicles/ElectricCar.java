7219c81911c3ec3a95d26284ae4d0cba3551409e
Anita Safar <ancsa0416@gmail.com>
2022-01-11 14:44:12 +0000

    electricCar setup done, getters and setters added

---
+++ src/main/java/vehicles/ElectricCar.java
@@ -0,0 +1,80 @@
+package vehicles;
+
+import components.*;
+
+public class ElectricCar {
+
+    private String petrolType;
+    private Windscreen windscreen;
+    private SteeringWheel steeringWheel;
+    private Engine engine;
+    private Tyre tyre;
+    private Battery battery;
+    private BrakePad brakePad;
+
+    public ElectricCar(String petrolType, Windscreen windscreen, SteeringWheel steeringWheel, Engine engine, Tyre tyre, Battery battery, BrakePad brakePad) {
+        this.petrolType = petrolType;
+        this.windscreen = windscreen;
+        this.steeringWheel = steeringWheel;
+        this.engine = engine;
+        this.tyre = tyre;
+        this.battery = battery;
+        this.brakePad = brakePad;
+    }
+
+    public String getPetrolType() {
+        return petrolType;
+    }
+
+    public void setPetrolType(String petrolType) {
+        this.petrolType = petrolType;
+    }
+
+    public Windscreen getWindscreen() {
+        return windscreen;
+    }
+
+    public void setWindscreen(Windscreen windscreen) {
+        this.windscreen = windscreen;
+    }
+
+    public SteeringWheel getSteeringWheel() {
+        return steeringWheel;
+    }
+
+    public void setSteeringWheel(SteeringWheel steeringWheel) {
+        this.steeringWheel = steeringWheel;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Tyre getTyre() {
+        return tyre;
+    }
+
+    public void setTyre(Tyre tyre) {
+        this.tyre = tyre;
+    }
+
+    public Battery getBattery() {
+        return battery;
+    }
+
+    public void setBattery(Battery battery) {
+        this.battery = battery;
+    }
+
+    public BrakePad getBrakePad() {
+        return brakePad;
+    }
+
+    public void setBrakePad(BrakePad brakePad) {
+        this.brakePad = brakePad;
+    }
+}
